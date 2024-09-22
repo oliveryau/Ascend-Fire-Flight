2eb2703b75efbb578621132b40d5e479c1b2fe37
Oliver Yau <55869156+oliveryau@users.noreply.github.com>
2024-09-23 02:33:45 +0800

    basic audio manager setup wip

--- Assets/_Scripts/_AudioManager.cs
+++ Assets/_Scripts/_AudioManager.cs
@@ -1,16 +1,68 @@
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AudioManager Instance;
+
+    [System.Serializable]
+    public class Sound
     {
-        
+        public string name;
+        public AudioClip clip;
+
+        [Range(0f, 1f)] public float volume;
+        [Range(0.1f, 3f)] public float pitch;
+
+        public bool loop;
+        public float spatialBlend = 1f;
+
+        [HideInInspector] public AudioSource AudioSource;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Sound[] sounds;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        AddAudioSource();
+    }
+
+    private void Start()
+    {
+        //Play("Main BGM");
+    }
+
+    private void AddAudioSource()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.AudioSource = gameObject.AddComponent<AudioSource>();
+            s.AudioSource.clip = s.clip;
+            s.AudioSource.volume = s.volume;
+            s.AudioSource.pitch = s.pitch;
+            s.AudioSource.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + "not found");
+            return;
+        }
+        s.AudioSource.Play();
     }
 }
